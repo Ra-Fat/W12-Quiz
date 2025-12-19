1184da32f3cde6398ad37912615ad195e9319c62
RaFat <manrafat2006@gmail.com>
2025-12-19 14:00:02 +0700

    finished

---
+++ lib/data/mock_grocery_repository.dart
@@ -0,0 +1,20 @@
+import '../models/grocery.dart';
+// import 'uuid/uuid.dart';
+
+final dummyGroceryItems = [
+   Grocery(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: GroceryCategory.dairy),
+   Grocery(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: GroceryCategory.fruit),
+   Grocery(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: GroceryCategory.meat),
+];
